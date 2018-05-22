efd71ebdbbcf35c5075c4f98912ed58036ad9127
khadijabibi <khadija.b.khan@gmail.com>
2018-05-23 07:22:11 +1200

    Add files via upload

---
+++ Staff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EpicConstruction.ViewAdminModels
+{
+    public class Staff
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }        
+        public string Voting { get; set; }
+        public DateTime VotedDate { get; set; }
+        
+
+
+        public ICollection<Candidate> Candidates { get; set; }
+    }
+}
