216eff14ba58ad788fd9bd823057ea527588eae9
khadijabibi <khadija.b.khan@gmail.com>
2018-05-23 07:24:07 +1200

    Add files via upload

---
+++ CandidatesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EpicConstruction.Models.Data;
+using EpicConstruction.ViewAdminModels;
+
+namespace EpicConstruction.Controllers
+{
+    public class CandidatesController : Controller
+    {
+        private DBAdminModel db = new DBAdminModel();
+
+        // GET: Candidates
+        public ActionResult Index()
+        {
+            var candidates = db.Candidates.Include(c => c.Staff);
+            return View(candidates.ToList());
+        }
+
+        // GET: Candidates/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Candidate candidate = db.Candidates.Find(id);
+            if (candidate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(candidate);
+        }
+
+        // GET: Candidates/Create
+        public ActionResult Create()
+        {
+            ViewBag.StaffID = new SelectList(db.Staffs, "ID", "FirstMidName");
+            return View();
+        }
+
+        // POST: Candidates/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,StaffID,FirstName,LastName,Department,Votes")] Candidate candidate)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Candidates.Add(candidate);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.StaffID = new SelectList(db.Staffs, "ID", "FirstMidName", candidate.StaffID);
+            return View(candidate);
+        }
+
+        // GET: Candidates/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Candidate candidate = db.Candidates.Find(id);
+            if (candidate == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.StaffID = new SelectList(db.Staffs, "ID", "FirstMidName", candidate.StaffID);
+            return View(candidate);
+        }
+
+        // POST: Candidates/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,StaffID,FirstName,LastName,Department,Votes")] Candidate candidate)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(candidate).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.StaffID = new SelectList(db.Staffs, "ID", "FirstMidName", candidate.StaffID);
+            return View(candidate);
+        }
+
+        // GET: Candidates/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Candidate candidate = db.Candidates.Find(id);
+            if (candidate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(candidate);
+        }
+
+        // POST: Candidates/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Candidate candidate = db.Candidates.Find(id);
+            db.Candidates.Remove(candidate);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
